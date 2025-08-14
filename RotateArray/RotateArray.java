c66cfa4bd3e122bb5e8d65b58724a174712ee13b
Mustapha Boutoub | kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-14 12:28:40 +0100

    fin

--- RotateArray/RotateArray.java
+++ RotateArray/RotateArray.java
@@ -1,31 +1,37 @@
 public class RotateArray {
     public static Integer[] rotate(Integer[] arr, int rotationCount) {
-        if (arr.length == 0)
-            return arr;
-        if (rotationCount > 0) {
-            while (rotationCount > 0) {
-                int temp = arr[arr.length - 1];
-                for (int i = 0; i < arr.length; i++) {
-                    int reset = arr[i];
-                    arr[i] = temp;
-                    temp = reset;
-                }
-                rotationCount--;
-            }
-        } else {
-            while (rotationCount < 0) {
-                int temp = arr[0];
-                for (int i = arr.length - 1; i > 0; i--) {
-                    System.out.println(arr[i]);
-                    int reset = arr[i];
-                    arr[i] = temp;
-                    temp = reset;
-                }
-                arr[0] = temp;
-                rotationCount++;
-            }
+        // if (arr.length == 0)
+        // return arr;
+        // if (rotationCount > 0) {
+        // while (rotationCount > 0) {
+        // int temp = arr[arr.length - 1];
+        // for (int i = 0; i < arr.length; i++) {
+        // int reset = arr[i];
+        // arr[i] = temp;
+        // temp = reset;
+        // }
+        // rotationCount--;
+        // }
+        // } else {
+        // while (rotationCount < 0) {
+        // int temp = arr[0];
+        // for (int i = arr.length - 1; i > 0; i--) {
+        // System.out.println(arr[i]);
+        // int reset = arr[i];
+        // arr[i] = temp;
+        // temp = reset;
+        // }
+        // arr[0] = temp;
+        // rotationCount++;
+        // }
+        // }
+        // return arr;
+
+        Integer[] new_arr = new Integer[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            new_arr[Math.floorMod(rotationCount + i, arr.length)] = arr[i];
         }
-        return arr;
+        return new_arr;
 
     }
 }
