457683763f7b541b0789ae1b58012a427773b145
Mustapha Boutoub | kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-14 13:45:10 +0100

    fin

--- NextPrime/NextPrime.java
+++ NextPrime/NextPrime.java
@@ -1,5 +1,22 @@
-package NextPrime;
-
 public class NextPrime {
-    
+    public static Integer nextPrime(Integer n) {
+        if (n <= 1) {
+            n = 1;
+        }
+        while (true) {
+            n++;
+            if (isPrime(n)) {
+                return n;
+            }
+        }
+    }
+
+    public static boolean isPrime(Integer n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
