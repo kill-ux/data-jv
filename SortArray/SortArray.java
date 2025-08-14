aefceb775a5faeb808d35af6e877b44d00537cdd
Mustapha Boutoub | kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-14 13:25:55 +0100

    fin

--- SortArray/SortArray.java
+++ SortArray/SortArray.java
@@ -1,14 +1,16 @@
+import java.util.*;
 public class SortArray {
     public static Integer[] sort(Integer[] args) {
-        for (int i = 0; i < args.length - 1; i++) {
-            for (int j = i + 1; j < args.length; j++) {
-                if (args[i] > args[j]) {
-                    int temp = args[i];
-                    args[i] = args[j];
-                    args[j] = temp;
-                }
-            }
-        }
+        // for (int i = 0; i < args.length - 1; i++) {
+        //     for (int j = i + 1; j < args.length; j++) {
+        //         if (args[i] > args[j]) {
+        //             int temp = args[i];
+        //             args[i] = args[j];
+        //             args[j] = temp;
+        //         }
+        //     }
+        // }
+        Arrays.sort(args);
         return args;
     }
 }
