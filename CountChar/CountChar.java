f153c28009701eda037357d6dbcf30fccd5b8673
Mustapha Boutoub | kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-14 11:50:14 +0100

    fin

--- CountChar/CountChar.java
+++ CountChar/CountChar.java
@@ -1,8 +1,11 @@
 public class CountChar {
     public static int count(String s, char c) {
-        
+        int inc = 0;
         for (int i = 0; i < s.length() ; i++) {
-
+            if (s.charAt(i) == c) {
+                inc++;
+            }
         }
+        return inc;
     }
 }
