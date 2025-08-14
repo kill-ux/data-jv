c15fc796e64419b9aa5a5cabe471896075c2c90a
Mustapha Boutoub | kill-ux <mustaphaboutoubdev@gmail.com>
2025-08-14 12:36:59 +0100

    fin

---
+++ ReverseArray/ReverseArray.java
@@ -0,0 +1,10 @@
+public class ReverseArray {
+    public static Integer[] reverse(Integer[] arr) {
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
+        return arr;
+    }
+}
